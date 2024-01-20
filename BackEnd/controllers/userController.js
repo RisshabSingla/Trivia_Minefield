7ab39f1236839856c61b933665e2b09588bf9714
Risshab Singla <risshab2singla@gmail.com>
2024-01-20 14:01:29 +0530

    Basic User Routes

---
+++ BackEnd/controllers/userController.js
@@ -0,0 +1,88 @@
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err,
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err,
+    });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findOne(req.params.id);
+    return res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err,
+    });
+  }
+};
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({
+      status: "success",
+      result: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: "failed",
+      message: err,
+    });
+  }
+};
