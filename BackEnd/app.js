2ec87eaeeb9ff5db612d23ce233332b5ec89313b
Risshab Singla <risshab2singla@gmail.com>
2024-06-01 21:12:44 +0530

    Cookies Allowed for CORS

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
