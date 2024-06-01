7304b92c0dae7b6c069add9558030ed4fccd642b
Risshab Singla <risshab2singla@gmail.com>
2024-06-01 20:41:13 +0530

    CORS Policy Update

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -23,6 +23,16 @@ app.use(
 );
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.get("/api", (req, res) => {
   // console.log("Cookies: ", req.cookies);
   res.status(200).send("Hello from backend");
